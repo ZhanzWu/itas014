ebf3e933862019e31814abbc6bd611bc865f69c5
使用者 <@LAPTOP-EOFH6302>
2023-03-21 21:11:06 +0800

    加入專案檔案。

---
+++ 014.cpp
@@ -0,0 +1,21 @@
+﻿// 014.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string st,st2;
+    cin >> st;
+    int i;
+    for (i = st.size()-1;i>=0; i--)
+    {
+        st2 += st[i];
+    }
+    if (st2 == st) { cout << "YES\n"; }
+    else
+    {
+        cout << "NO\n";
+    }
+}
